0c47d03f56a33b8bb12512cb1d19421fc7f29d65
Temuulen8 <b.temuulen2005@gmail.com>
2024-06-10 12:56:42 +0800

    javascript

---
+++ js_basics/main.js
@@ -0,0 +1,63 @@
+//variable
+
+/*this is 
+multiline comment*/
+
+// let ner = "Temuulen"; //string
+// let nas = 19; //number 20, 20.3
+// let gerlsenEseh = true; // boolean: true/false
+
+// let job;
+// let angi = null;
+
+// console.log("tanii ner: ", ner);
+// console.log("tanii nas: ", nas);
+// console.log("tanii gerleltiin baidal: ", gerlsenEseh);
+// console.log("tanii ajil:", job);
+// console.log("tanii angi:", angi);
+
+//bodlogo 1
+let ner = "Temuulen";
+console.log("tanii ner:", ner);
+
+//bodlogo 2
+let firstname = "Temuulen";
+let lastname = "Byambadorj";
+let country = "Mongolia";
+let email = "b.temuulen2005@gmai.com";
+console.log("uuriin ner:", firstname);
+console.log("aaviin ner:", lastname);
+console.log("tursun gazar:", country);
+console.log("email chin yu vee:", email);
+
+//bodlogo 3
+let friendname = "Tselmeg"; //string
+let nas = 19; //number 20, 20.3
+let gerlsenEseh = true; //boolean: true/false
+console.log("naiziin ner: ", ner);
+console.log("tanii nas: ", nas);
+console.log("tanii gerleltiin baidal: ", gerlsenEseh);
+
+//bodlogo 4
+let x = 4;
+let y = 8;
+let add = x + y; //12
+let minus = y - x; //4
+let times = x * y; //32
+let devide = y / x; //2
+
+console.log(" x y hoyoriin niilber ", add);
+console.log(" x y hoyoriin yalgavar ", add);
+console.log(" x y hoyoriin urjver ", add);
+console.log(" x y hoyoriin huvaah ", add);
+
+//bodlogo 5
+let number1 = 1,
+  number2 = 2,
+  number3 = 3;
+let p = a + b + c; //6
+
+//bodlogo 6
+// let 8 * 1024 * 1024 * 1024
+
+//bodlogo 7
