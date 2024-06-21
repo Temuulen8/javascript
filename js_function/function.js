b6bf55bd60f8e17f15b44a97753c28a797967f87
Temuulen8 <b.temuulen2005@gmail.com>
2024-06-21 14:36:17 +0800

    d

--- js_function/function.js
+++ js_function/function.js
@@ -1,15 +1,83 @@
-function greet() {
-  console.log("hello there");
-}
-greet();
+// function greet() {
+//   console.log("hello there");
+// }
+// greet();
 
-function name(name) {
-  console.log("hello " + name + " :)");
-}
-let input = prompt("enter name");
-name(input);
+// function name(name) {
+//   console.log("hello " + name + " :)");
+// }
+// let input = prompt("enter name");
+// name(input);
 
-function myfunction(p1, p2) {
-  return p1 * p2;
+function myfunction(p1, p2, p3) {
+  let multiply = p1 * p2 * p3;
+
+  return multiply;
 }
-myfunction(2, 3);
+
+console.log(myfunction(2, 3, 4));
+
+function findSum(num1, num2) {
+  let sum = num1 + num2;
+  // console.log("num1", num1);
+  // console.log("sum", sum);
+  return sum;
+  // console.log("sum++", sum);
+}
+
+console.log("sum", findSum(7, 8));
+// findSum(7, 8);
+
+//arrow function
+let sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log("sum with arrow", sum(2, 3));
+
+// exercise 1
+function age(myage) {
+  if (myage >= 25) {
+    return myage;
+    // console.log("above 25 ", myage);
+  } else {
+    return "below 25";
+    // console.log("below 25", myage);
+  }
+}
+
+console.log(age(20));
+
+let sum1 = 0;
+let even_sum = 0;
+let array = [1, 2, 3, 4, 5];
+function arraysum() {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    sum1 = sum1 + array[i];
+    // console.log("kk", sum1);
+    if (array[i] % 2 == 0) {
+      even_sum = even_sum + array[i];
+      console.log(even_sum);
+    }
+  }
+  return sum1;
+}
+
+console.log(arraysum());
+
+function findnum(nums, find) {
+  // console.log(nums);
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] === find) {
+      // console.log("tentsej bn", nums[j]);
+      return nums[j];
+    }
+  }
+  return -1;
+}
+
+let number = [3, 4, 6, 1];
+
+// findnum(number, 11);
+console.log(findnum(number, 6));
---
+++ js_object/object.js
@@ -0,0 +1,69 @@
+// console.log("student", student.gender);
+// console.log("name", student.name);
+
+function studentInfo(student) {
+  return student.name + student.gender;
+}
+
+let studentData = {
+  name: "Temuulen",
+  phone: "9977",
+  gender: "male",
+};
+console.log(studentInfo(studentData));
+
+function studentsage(students) {
+  for (k = 0; k < students.length; k++) {
+    console.log(students[k].age);
+  }
+}
+
+let students = [
+  {
+    name: "Сэд-Эрдэнэ",
+    age: 19,
+    gender: "male",
+    points: 70,
+  },
+  {
+    name: "Индра",
+
+    age: 19,
+
+    gender: "female",
+
+    points: 50,
+  },
+
+  {
+    name: "Хатнаа ",
+
+    age: 21,
+
+    gender: "male",
+
+    points: 99,
+  },
+
+  {
+    name: "Тэмүүлэн",
+
+    age: 23,
+
+    gender: "male",
+
+    points: 55,
+  },
+
+  {
+    name: "Намуун",
+
+    age: 23,
+
+    gender: "female",
+
+    points: 85,
+  },
+];
+studentsage(students);
+// console.log(students[k].age);
